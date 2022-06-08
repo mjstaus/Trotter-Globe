524e101e0e6bb640475c20bcf665cf499066c735
Martha Staus <mjstaus@gmail.com>
2022-06-08 10:19:36 -0500

    chore: clean up tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,22 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'shrikhand': ['shrikhand', 'cursive'],
-        'urbanist': ['urbanist', 'sans-serif']
-      },
       animation: {
         'spin-slow': 'spin 2.3s linear infinite',
-      },
-      scale: {
-        '10003': '1.003',
-        '102': '1.02',
       },
       boxShadow: {
         'custom-sm': '1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 3px 6px 6px hsl(220deg 60% 50% / 0.333)',
         'custom-med': '1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 4px 8px 8px hsl(220deg 60% 50% / 0.333), 8px 16px 16px hsl(220deg 60% 50% / 0.333)',
         'custom-lg': '1px 2px 2px hsl(220deg 60% 50% / 0.2), 2px 4px 4px hsl(220deg 60% 50% / 0.2), 4px 8px 8px hsl(220deg 60% 50% / 0.2), 8px 16px 16px hsl(220deg 60% 50% / 0.2),16px 32px 32px hsl(220deg 60% 50% / 0.2)',
-      }
+      },
+      fontFamily: {
+        'shrikhand': ['shrikhand', 'cursive'],
+        'urbanist': ['urbanist', 'sans-serif']
+      },
+      scale: {
+        '10003': '1.003',
+        '102': '1.02',
+      },
     },
   },
   plugins: [require("daisyui")],
