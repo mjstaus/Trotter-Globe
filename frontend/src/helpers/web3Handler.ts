eed44345ad069a733e1fd47509ba705c074b7986
Martha Staus <mjstaus@gmail.com>
2022-09-04 16:45:53 -0700

    feature: add alert to switch to correct network (currently mumbai)

--- frontend/src/helpers/web3Handler.ts
+++ frontend/src/helpers/web3Handler.ts
@@ -1,8 +1,15 @@
-import { ethers } from "ethers";
+import { providers } from "ethers";
 
 export default async function web3Handler() {
   // Get provider from Metamask
-  const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+  const provider = new providers.Web3Provider(window.ethereum as any);
+  
+  const { chainId } = await provider.getNetwork();
+  if (chainId !== 80001) {
+    window.alert("Please switch to the Polygon network!");
+    throw new Error("Please switch to the Polygon network");
+  }
+
   // Set signer
   const signer = provider.getSigner();
 
