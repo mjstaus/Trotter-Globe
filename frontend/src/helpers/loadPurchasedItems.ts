9bb0410c48ee3fbd097c092763a1ceee0be5790b
Martha Staus <mjstaus@gmail.com>
2022-09-06 16:15:33 -0700

    feature: Add owner property to shop item

--- frontend/src/helpers/loadPurchasedItems.ts
+++ frontend/src/helpers/loadPurchasedItems.ts
@@ -5,42 +5,34 @@ import loadContracts from "./loadContracts";
 export default async function loadPurchasedItems(account: string | undefined) {
 
   const { shop, token } = await loadContracts();
-  
+
+  let purchasedItems = [];
   try {
-    // Fetch purchased items from shop by quering Bought events with the buyer set as the user acct
-    const filter = shop.filters.Bought(
-      null,
-      null,
-      null,
-      null,
-      null,
-      account
-    );
-    const results = await shop.queryFilter(filter);
-    console.log("results", results)
-    //Fetch metadata of each token and add that to purchasedItems object.
-    const purchasedItems = await Promise.all(
-      results.map(async (d) => {
-        // fetch arguments from each result
-        const i = d.args;
+      const itemCount = await shop.callStatic.itemCount();
+  
+      for (let i = 1; i <= Number(itemCount.toString()); i++) {
+        const item = await shop.callStatic.items(i);
         // get uri url from token contract
-        const uri = await token.uri(i?.tokenId);
+        const uri = await token.uri(item.tokenId);
         // use uri to fetch the token metadata stored on ipfs
         const response = await axios.get(`https://gateway.pinata.cloud/ipfs/${uri}.json`);
         const metadata = await response.data;
-
-        let purchasedItem = {
-          price: ethers.utils.formatEther(i?.price),
-          itemId: i?.itemId,
-          name: metadata?.name,
-          description: metadata?.description,
-          image: `https://gateway.pinata.cloud/ipfs/${metadata?.image}.png`,
-        };
-        return purchasedItem;
-      })
-    );
-    return purchasedItems;
-  } catch (error) {
-    console.log("Error", error);
-  }
+  
+        if (item.owner === account) {
+          // Add item to items array
+          
+          purchasedItems.push({
+            price: ethers.utils.formatEther(item?.price),
+            owner: item?.owner,
+            itemId: item?.itemId,
+            name: metadata?.name,
+            description: metadata?.description,
+            image: `https://gateway.pinata.cloud/ipfs/${metadata?.image}.png`,
+          });
+        }
+      }
+      return purchasedItems;
+    } catch (error) {
+      console.log("Error", error);
+    }
 };
