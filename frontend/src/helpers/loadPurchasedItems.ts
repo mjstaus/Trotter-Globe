7f9b416d9761b916ae0870069051b47362680edd
Martha Staus <mjstaus@gmail.com>
2022-09-06 17:26:34 -0700

    feature: loadPurchasedItems helper populates purchasedItems array based on item.owner property

--- frontend/src/helpers/loadPurchasedItems.ts
+++ frontend/src/helpers/loadPurchasedItems.ts
@@ -1,38 +1,39 @@
 import { ethers } from "ethers";
-import axios  from "axios";
+import axios from "axios";
 import loadContracts from "./loadContracts";
 
 export default async function loadPurchasedItems(account: string | undefined) {
-
   const { shop, token } = await loadContracts();
 
   let purchasedItems = [];
   try {
-      const itemCount = await shop.callStatic.itemCount();
-  
-      for (let i = 1; i <= Number(itemCount.toString()); i++) {
-        const item = await shop.callStatic.items(i);
-        // get uri url from token contract
-        const uri = await token.uri(item.tokenId);
-        // use uri to fetch the token metadata stored on ipfs
-        const response = await axios.get(`https://gateway.pinata.cloud/ipfs/${uri}.json`);
-        const metadata = await response.data;
-  
-        if (item.owner === account) {
-          // Add item to items array
-          
-          purchasedItems.push({
-            price: ethers.utils.formatEther(item?.price),
-            owner: item?.owner,
-            itemId: item?.itemId,
-            name: metadata?.name,
-            description: metadata?.description,
-            image: `https://gateway.pinata.cloud/ipfs/${metadata?.image}.png`,
-          });
-        }
+    const itemCount = await shop.callStatic.itemCount();
+
+    for (let i = 1; i <= Number(itemCount.toString()); i++) {
+      const item = await shop.callStatic.items(i);
+      // get uri url from token contract
+      const uri = await token.uri(item.tokenId);
+      // use uri to fetch the token metadata stored on ipfs
+      const response = await axios.get(
+        `https://gateway.pinata.cloud/ipfs/${uri}.json`
+      );
+      const metadata = await response.data;
+
+      if (item.owner === account) {
+        // Add item to items array
+        console.log("item", item);
+        purchasedItems.push({
+          price: ethers.utils.formatEther(item?.price),
+          owner: item?.owner,
+          itemId: item?.itemId,
+          name: metadata?.name,
+          description: metadata?.description,
+          image: `https://gateway.pinata.cloud/ipfs/${metadata?.image}.png`,
+        });
       }
-      return purchasedItems;
-    } catch (error) {
-      console.log("Error", error);
     }
-};
+    return purchasedItems;
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
